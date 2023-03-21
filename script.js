65bb5ca85aa5acf4fe339070c19694fa4e1f7bf1
headphoneJac <71986880+headphoneJac@users.noreply.github.com>
2023-03-21 19:18:55 +0800

    Integrate firebase database to customerSignUp

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-import { initializeApp } from 'firebase/app';
-
-const firebaseApp = initializeApp({
-    apiKey: "AIzaSyBqyyKsQ3Wk6reiLm8k9qBAuz59cZk3PbU",
-    authDomain: "cpecaps-project.firebaseapp.com",
-    databaseURL: "https://cpecaps-project-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "cpecaps-project",
-    storageBucket: "cpecaps-project.appspot.com",
-    messagingSenderId: "1002343899498",
-    appId: "1:1002343899498:web:125ea29ca0d15d98090513",
-    measurementId: "G-0XXL384QBN"
-});
-
-function verifyLogin() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    if (email === "user@gmail.com" && password === "web_dev"){
-        alert("You have successfully logged in.");
-        window.location.href = "home.html";
-    }
-    else {
-        alert("Invalid username and/or password");
-        location.reload();
-    }
-}
-
-
 class menuItem {
     constructor(name, price, image) {
       this.name = name;
@@ -106,23 +78,10 @@ function subPrice(){
     document.getElementById('price').innerHTML = "PhP " + newPrice.toFixed(2);
 }
 
-
-
-
 function addOrder(){
     /* add the product to the order details*/
     var email = document.getElementById('email');
     var order_quantity = document.getElementById('amount');
     var order_name = document.getElementById('itemName');
     
-}
-
-function customerSignUp(){
-    var firstname = document.getElementById('firstname');
-    var surname = document.getElementById('surname');
-    var email = document.getElementById('email');
-    var contactNo = document.getElementById('contactNo');
-    var address = document.getElementById('address');
-    var password = document.getElementById('password');
-    var confirmPassword = document.getElementById('confirmPassword');
 }
