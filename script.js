507120ab372e2655b6d3626d056bc3da173c79e6
Neilz05 <neil_dong@dlsu.edu.ph>
2023-03-21 21:35:01 +0800

    -changed "sisigParty.png" to "sisigAlaCarteParty.png" -added onclick functionalities to all menu items in menu.html -changed the nav bar in menu and selectitem.html - added new menu items in javascript

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ class menuItem {
 const mangoStickyRice = new menuItem("Mango Sticky Rice", 90, "images/mangoStickyRice.png");
 const sisigAlaCarte = new menuItem("Sisig ala Carte", 250, "images/sisigAlaCarte.png");
 const bakedSisigAlaCarte = new menuItem("Baked Sisig ala Carte", 300, "images/bakedSisigAlaCarte.png");
-
+const mangoStickyRiceParty = new menuItem("Mango Sticky Rice Party", 250, "images/mangoStickyRiceParty.png");
+const sisigAlaCarteParty = new menuItem("Sisig ala Carte Party", 700, "images/sisigAlaCarteParty.png");
+const bakedSisigParty = new menuItem("Baked Sisig ala Carte Party", 1100, "images/bakedSisigParty.png");
+const bundleA = new menuItem("Bundle A", 1340, "images/bundleA.png");
+const bundleB = new menuItem("Bundle B", 2120, "images/bundleB.png");
 
 function menuImage1Click(){
     localStorage.setItem("itemPrice", mangoStickyRice.getPrice());
@@ -41,17 +45,68 @@ function menuImage3Click(){
     window.location.href = "selectItem.html";
 }
 
+function menuImage4Click(){
+    localStorage.setItem("itemPrice", mangoStickyRiceParty.getPrice());
+    localStorage.setItem("itemName", mangoStickyRiceParty.getName());    
+    localStorage.setItem("imageToDisplay", mangoStickyRiceParty.getImage());
+    window.location.href = "selectItem.html";
+}
+
+function menuImage5Click(){
+    localStorage.setItem("itemPrice", sisigAlaCarteParty.getPrice());
+    localStorage.setItem("itemName", sisigAlaCarteParty.getName());    
+    localStorage.setItem("imageToDisplay", sisigAlaCarteParty.getImage());
+    window.location.href = "selectItem.html";
+}
+
+function menuImage6Click(){
+    localStorage.setItem("itemPrice", bakedSisigParty.getPrice());
+    localStorage.setItem("itemName", bakedSisigParty.getName());    
+    localStorage.setItem("imageToDisplay", bakedSisigParty.getImage());
+    window.location.href = "selectItem.html";
+}
+
+function menuImage7Click(){
+    localStorage.setItem("itemPrice", bundleA.getPrice());
+    localStorage.setItem("itemName", bundleA.getName());    
+    localStorage.setItem("imageToDisplay", bundleA.getImage());
+    window.location.href = "selectItem.html";
+}
+
+function menuImage8Click(){
+    localStorage.setItem("itemPrice", bundleB.getPrice());
+    localStorage.setItem("itemName", bundleB.getName());    
+    localStorage.setItem("imageToDisplay", bundleB.getImage());
+    window.location.href = "selectItem.html";
+}
+
 function addPrice() {   
-    
-    if (document.getElementById("itemName").innerHTML== mangoStickyRice.getName()){
-        var price = mangoStickyRice.getPrice();
+    switch(document.getElementById("itemName").innerHTML){
+        case mangoStickyRice.getName():
+            var price = mangoStickyRice.getPrice();
+            break;
+        case sisigAlaCarte.getName():
+            var price = sisigAlaCarte.getPrice();
+            break;
+        case bakedSisigAlaCarte.getName():
+            var price = bakedSisigAlaCarte.getPrice();
+            break;
+        case mangoStickyRiceParty.getName():
+            var price = mangoStickyRiceParty.getPrice(); 
+            break;
+        case sisigAlaCarteParty.getName():
+            var price = sisigAlaCarteParty.getPrice(); 
+            break;
+        case bakedSisigParty.getName():
+            var price = bakedSisigParty.getPrice(); 
+            break;
+        case bundleA.getName():
+            var price = bundleA.getPrice(); 
+            break;
+        case bundleB.getName():
+            var price = bundleB.getPrice(); 
+            break;        
     }
-    else if (document.getElementById("itemName").innerHTML == sisigAlaCarte.getName()){
-        var price = sisigAlaCarte.getPrice();
-    }
-    else if(document.getElementById("itemName").innerHTML == bakedSisigAlaCarte.getName()){
-        var price = bakedSisigAlaCarte.getPrice();  
-    }  
     var amount = document.getElementById('amount');
     var currAmount = parseInt(amount.innerHTML);
     var newAmount = currAmount + 1;    
@@ -60,7 +115,32 @@ function addPrice() {
     document.getElementById('price').innerHTML =  "PhP " + newPrice.toFixed(2);
 }
 function subPrice(){
-    
+    switch(document.getElementById("itemName").innerHTML){
+        case mangoStickyRice.getName():
+            var price = mangoStickyRice.getPrice();
+            break;
+        case sisigAlaCarte.getName():
+            var price = sisigAlaCarte.getPrice();
+            break;
+        case bakedSisigAlaCarte.getName():
+            var price = bakedSisigAlaCarte.getPrice();
+            break;
+        case mangoStickyRiceParty.getName():
+            var price = mangoStickyRiceParty.getPrice(); 
+            break;
+        case sisigAlaCarteParty.getName():
+            var price = sisigAlaCarteParty.getPrice(); 
+            break;
+        case bakedSisigParty.getName():
+            var price = bakedSisigParty.getPrice(); 
+            break;
+        case bundleA.getName():
+            var price = bundleA.getPrice(); 
+            break;
+        case bundleB.getName():
+            var price = bundleB.getPrice(); 
+            break;        
+    }
     if (document.getElementById("itemName").innerHTML == mangoStickyRice.getName()){
         var price = mangoStickyRice.getPrice();
     }
